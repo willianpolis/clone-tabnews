c9b5d61f28affc975de19388e461d0a0c16f8f4d
Willian Polis <polis@pp.business>
2024-04-21 16:02:09 +0000

    adiciona scripts e

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Pena de urubu, pena de galinha, se voce ja deu seu cu, de uma risadinha.</h1>
+  return (
+    <h1>
+      Pena de urubu, pena de galinha, se voce ja deu seu cu, de uma risadinha.
+    </h1>
+  );
 }
 
 export default Home;
