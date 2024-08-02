f3e59cd1fa7d56a2a3bbf262fa8f91bd483b4a12
Willian Polis <polis@pp.business>
2024-08-02 19:12:50 +0000

    Adiçao das variáveis de ambiente de conexão com Postgres

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("Select 1+1;");
+  console.log(result);
   response
     .status(200)
     .json({ chave: "Alunos do curso.dev são pessoas acima da média" });
