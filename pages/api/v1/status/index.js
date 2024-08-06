f3a8fe898aee6d59a20772940a9f011faecfe8ff
Willian Polis <polis@pp.business>
2024-08-06 11:12:44 +0000

    makes `database.js` more robust to errors

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,35 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("Select 1+1;");
-  console.log(result);
-  response
-    .status(200)
-    .json({ chave: "Alunos do curso.dev são pessoas acima da média" });
+  const updatedAt = new Date().toISOString();
+  const postgresVersion = await getPostgresVersion();
+  const maxConnections = await getMaxConnections();
+  const usedConnections = await getUsedConnections();
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    postgres_version: postgresVersion,
+    max_connections: maxConnections,
+    used_connections: usedConnections,
+  });
+}
+
+async function getPostgresVersion() {
+  const result = await database.query("SELECT version()");
+  const version = await result.rows[0].version;
+  return version;
+}
+
+async function getMaxConnections() {
+  const result = await database.query("SHOW max_connections");
+  const maxConnections = await result.rows[0].max_connections;
+  return maxConnections;
+}
+
+async function getUsedConnections() {
+  const result = await database.query("SELECT COUNT(*) FROM pg_stat_activity");
+  const usedConnections = await result.rows[0].count;
+  return usedConnections;
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,20 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  // Versão do postgres
+  const expectedPostgresVersion =
+    "PostgreSQL 16.0 on x86_64-pc-linux-musl, compiled by gcc (Alpine 12.2.1_git20220924-r10) 12.2.1 20220924, 64-bit";
+  expect(responseBody.postgres_version).toBe(expectedPostgresVersion);
+
+  // Maximo de Conexoes
+  const mockMaxConnections = "100";
+  expect(responseBody.max_connections).toBe(mockMaxConnections);
+
+  // Conexoes em uso
+  const mockConnectionsInUse = "5";
+  //expect(responseBody.used_connections).toBe(mockConnectionsInUse);
 });
