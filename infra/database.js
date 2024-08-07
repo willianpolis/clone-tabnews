1de4eeaac6848d783564082f21a9d5074a0e34d6
Willian Polis <polis@pp.business>
2024-08-07 11:39:25 +0000

    add migrations files

--- infra/database.js
+++ infra/database.js
@@ -10,15 +10,6 @@ async function query(queryObject) {
     ssl: getSSLValues(),
   });
 
-  console.log("Credenciais do postgres", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    ca: process.env.POSTGRES_CA,
-  });
-
   try {
     await client.connect();
     const result = await client.query(queryObject);
